3bcdc8093e63eb3d782e81ee38ac12d365306435
Icetrack <63660650+ICL32@users.noreply.github.com>
2023-04-10 22:53:49 +0300

    Clean up

--- Models/ItemsModel.cs
+++ Models/ItemsModel.cs
@@ -4,11 +4,9 @@ namespace Aphrodite.Models;
 
 public class ItemsModel
 {
-    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
     public float Cost { get; set; }
-    [Required]
     public byte[] ImageData { get; set; }
 }
 
